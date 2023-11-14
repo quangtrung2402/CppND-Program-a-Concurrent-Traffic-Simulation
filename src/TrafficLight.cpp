9bea4edfa40cc8c4879d7cda81163fcf578345e9
Quang Trung <quangtrung2402@gmail.com>
2023-11-15 00:32:23 +0700

    FP.3

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -5,7 +5,6 @@
 
 /* Implementation of class "MessageQueue" */
  
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
@@ -20,7 +19,6 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 }
-*/
 
 /* Implementation of class "TrafficLight" */
 
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,8 +19,12 @@ template <class T>
 class MessageQueue
 {
 public:
-
+    T receive();
+    void send(T &&msg);
 private:
+    std::deque<T> _queue;
+    std::condition_variable _condition;
+    std::mutex _mutex;
     
 };
 
